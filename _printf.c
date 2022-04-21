199cf70c6c7f1e9843a20027b0b93dfcd4267175
Jayode <97662506+Jayode@users.noreply.github.com>
2022-04-21 17:09:41 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,36 +2,81 @@
 #include <stdlib.h>
 
 /**
- * _printf - Function entry
- * Description: A function that produces output according to a format
- * @format: is a character string
- * @...: list of arguments
- * Return: the number of characters printed
- * (excluding the null byte used to end output to strings)
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
  */
-int _printf(const char *format, ...)
+static int (*check_for_specifiers(const char *format))(va_list)
 {
-	/**
-	 * unsigned int i = 0;
-	 * int return_count;
-	 */
-	int my_print = 0;
-	va_list args;
-	converter formatchar[] = {
-		{"s", _printString},
-		{"i", _printInt},
-		{"d", _printInt},
-		{"f", _printFloat},
-		{"c", _printChar},
-		{"%", _printCent},
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
 		{NULL, NULL}
 	};
 
-	va_start(args, format);
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
 
-	my_print = _print_specifier(format, formatchar, args);
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-	va_end(args);
-	
-	return (my_print);
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
 }
