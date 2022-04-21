12fc57cf81201cdc3f12c04c632007c08bdaa865
Jayode <97662506+Jayode@users.noreply.github.com>
2022-04-21 17:15:13 +0100

    Update _print_number.c

--- _print_number.c
+++ _print_number.c
@@ -1,39 +1,78 @@
 #include "main.h"
 
-/**
- * _print_number - Function entry
- * Description: prints a number send to this function
- * @args: List of arguments
- * Return: The number of arguments printed
+/** * print_i - prints an integer
+ * @i: integer to print
+ *
+ * Return: number of chars and digits printed
  */
-int _print_number(va_list args)
+int print_i(va_list i)
 {
-	int n;
-	int div;
-	int len;
-	unsigned int num;
+	int a[10];
+	int j, m, n, sum, count;
 
-	n  = va_arg(args, int);
-	div = 1;
-	len = 0;
-
+	n = va_arg(i, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
 	if (n < 0)
 	{
-		len += _putchar('-');
-		num = n * -1;
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
 	}
-	else
-		num = n;
-
-	for (; num / div > 9; )
-		div *= 10;
-
-	for (; div != 0; )
+	for (j = 0, sum = 0; j < 10; j++)
 	{
-		len += _putchar('0' + num / div);
-		num %= div;
-		div /= 10;
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
 	}
+	return (count);
+}
 
-	return (len);
+/**
+ * print_d - prints a decimal
+ * @d: decimal to print
+ *
+ * Return: number of chars and digits printed
+ */
+int print_d(va_list d)
+{
+	int a[10];
+	int j, m, n, sum, count;
+
+	n = va_arg(d, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
 }
